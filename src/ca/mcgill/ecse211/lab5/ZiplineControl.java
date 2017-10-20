ece8bdbd6c7ce490d2c603550f624e71b4bde404
Christophe Vauclair <cfvauclair@gmail.com>
2017-10-20 13:50:58 -0400

    Started ZiplineControl

---
+++ src/ca/mcgill/ecse211/lab5/ZiplineControl.java
@@ -0,0 +1,15 @@
+package ca.mcgill.ecse211.lab5;
+
+public class ZiplineControl {
+  public void grabZipline(){
+    
+  }
+  
+  public void advance(){
+    
+  }
+  
+  public void dismountZipline(){
+    
+  }
+}
